d3faa77bd708cd0adc5b0a544676117fd352ae39
Mahalingam97 <91451945+Mahalingam97@users.noreply.github.com>
2021-12-08 17:02:41 +0530

    Update cast.js

--- 4.12.2021/React/moviedetails/src/movie/cast.js
+++ 4.12.2021/React/moviedetails/src/movie/cast.js
@@ -1,56 +1,5 @@
-// import {movie} from "../moviedetails.js"
-// import "../assests/cast.css"
-// export default function MovieCast(){
-//     return(
-//         <div className="movie-cast-and-info">
-//         <p className="plot">{movie.Description}</p>
-//         <hr/>
-//         <p>CAST AND CREDITS</p>
-//         <div className="cast-detail">
-//           <div className="block">
-//             <p>Actors</p>
-//             <div><p>{movie.movieCast.Actors}</p></div>
-//           </div>
-//           <div className="block">
-//             <p>Producer</p>
-//             <div><p>{movie.movieCast.Producers}</p></div>
-//           </div>
-//           <div className="block">
-//             <p>Director</p>
-//             <div><p>{movie.movieCast.Director}</p></div>
-//           </div>
-//           <div className="block">
-//             <p>Writer</p>
-//             <div><p>{movie.movieCast.Writer}</p></div>
-//           </div>
-//         </div>
-//         <hr/>
-//         <p>ADDITIONAL INFORMATION</p>
-//         <div className="add-info">
-//           <div className="block">
-//             <p>Language</p>
-//             <p>{movie.Language}</p>
-//           </div>
-//           <div className="block">
-//             <p>Rating</p>
-//             <p>{movie.Rating}</p>
-//           </div>
-//           <div className="block">
-//             <p>Released on</p>
-//             <p>{movie.Released}</p>
-//           </div>
-//         </div>
-//       </div>
-//     )
-// }
-import { useState,useEffect } from "react";
 import "../assests/cast.css"
 export default function MovieCast(props){
-  // const [movie,setMovie]=useState([])
-  // useEffect(()=>{
-  //   setMovie(props)
-  // },[props])
-  console.log(props.movie);
   return(
         <div className="movie-cast-and-info">
         <p className="plot">{props.movie.Description}</p>
@@ -59,19 +8,19 @@ export default function MovieCast(props){
         <div className="cast-detail">
           <div className="block">
             <p>Actors</p>
-            <div><p>{props.movie.movieCast.Actors}</p></div>
+            <div><p>{props.cast.Actors}</p></div>
           </div>
           <div className="block">
             <p>Producer</p>
-            <div><p>{props.movie.movieCast.Producers}</p></div>
+            <div><p>{props.cast.Producers}</p></div>
           </div>
           <div className="block">
             <p>Director</p>
-            <div><p>{props.movie.movieCast.Director}</p></div>
+            <div><p>{props.cast.Director}</p></div>
           </div>
           <div className="block">
             <p>Writer</p>
-            <div><p>{props.movie.movieCast.Writer}</p></div>
+            <div><p>{props.cast.Writer}</p></div>
           </div>
         </div> 
         <hr/>
