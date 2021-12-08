c6e399be8315a93877c7235c97103ea43e6c3e86
Mahalingam97 <91451945+Mahalingam97@users.noreply.github.com>
2021-12-08 16:34:27 +0530

    Update cast.js

--- 4.12.2021/React/moviedetails/src/movie/cast.js
+++ 4.12.2021/React/moviedetails/src/movie/cast.js
@@ -1,43 +1,93 @@
-import {movie} from "../moviedetails.js"
+// import {movie} from "../moviedetails.js"
+// import "../assests/cast.css"
+// export default function MovieCast(){
+//     return(
+//         <div className="movie-cast-and-info">
+//         <p className="plot">{movie.Description}</p>
+//         <hr/>
+//         <p>CAST AND CREDITS</p>
+//         <div className="cast-detail">
+//           <div className="block">
+//             <p>Actors</p>
+//             <div><p>{movie.movieCast.Actors}</p></div>
+//           </div>
+//           <div className="block">
+//             <p>Producer</p>
+//             <div><p>{movie.movieCast.Producers}</p></div>
+//           </div>
+//           <div className="block">
+//             <p>Director</p>
+//             <div><p>{movie.movieCast.Director}</p></div>
+//           </div>
+//           <div className="block">
+//             <p>Writer</p>
+//             <div><p>{movie.movieCast.Writer}</p></div>
+//           </div>
+//         </div>
+//         <hr/>
+//         <p>ADDITIONAL INFORMATION</p>
+//         <div className="add-info">
+//           <div className="block">
+//             <p>Language</p>
+//             <p>{movie.Language}</p>
+//           </div>
+//           <div className="block">
+//             <p>Rating</p>
+//             <p>{movie.Rating}</p>
+//           </div>
+//           <div className="block">
+//             <p>Released on</p>
+//             <p>{movie.Released}</p>
+//           </div>
+//         </div>
+//       </div>
+//     )
+// }
+import { useState,useEffect } from "react";
 import "../assests/cast.css"
-export default function MovieCast(){
-    return(
+export default function MovieCast(props){
+  // const [movie,setMovie]=useState([])
+  // useEffect(()=>{
+  //   setMovie(props)
+  // },[props])
+  console.log(props.movie);
+  return(
         <div className="movie-cast-and-info">
-        <p className="plot">{movie.Description}</p>
+        <p className="plot">{props.movie.Description}</p>
         <hr/>
-        <p>CAST AND CREDITS</p>
+         <p>CAST AND CREDITS</p>
         <div className="cast-detail">
           <div className="block">
             <p>Actors</p>
-            <div><p>{movie.movieCast.Actors}</p></div>
+            <div><p>{props.movie.movieCast.Actors}</p></div>
           </div>
           <div className="block">
             <p>Producer</p>
-            <div><p>{movie.movieCast.Producers}</p></div>
+            <div><p>{props.movie.movieCast.Producers}</p></div>
           </div>
           <div className="block">
             <p>Director</p>
-            <div><p>{movie.movieCast.Director}</p></div>
+            <div><p>{props.movie.movieCast.Director}</p></div>
           </div>
           <div className="block">
             <p>Writer</p>
-            <div><p>{movie.movieCast.Writer}</p></div>
+            <div><p>{props.movie.movieCast.Writer}</p></div>
           </div>
-        </div>
+        </div> 
         <hr/>
         <p>ADDITIONAL INFORMATION</p>
         <div className="add-info">
           <div className="block">
             <p>Language</p>
-            <p>{movie.Language}</p>
+            <p>{props.movie.Language}</p>
           </div>
           <div className="block">
             <p>Rating</p>
-            <p>{movie.Rating}</p>
+            <p>{props.movie.Rating}</p>
           </div>
           <div className="block">
             <p>Released on</p>
-            <p>{movie.Released}</p>
+            <p>{props.movie.Released}</p>
           </div>
         </div>
       </div>
